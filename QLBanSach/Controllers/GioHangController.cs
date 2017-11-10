efa150835dca7ae000693f9651ac9dbd0e0e7cc8
SKG310H69WOBENJ\Administrator <levietquang230996@gmail.com>
2017-11-10 13:55:27 +0700

    update
    Giỏ hàng

---
+++ QLBanSach/Controllers/GioHangController.cs
@@ -0,0 +1,113 @@
+﻿using QLBanSach.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QLBanSach.Controllers
+{
+    public class GioHangController : Controller
+    {
+        QLBS db = new QLBS();
+        public List<GioHang> LayGioHang()
+        {
+            List<GioHang> lstGioHang = 
+                Session["GioHang"] as List<GioHang>;
+            if (lstGioHang == null)
+            {
+                lstGioHang = new List<GioHang>();
+                Session["GioHang"] = lstGioHang;
+
+            }
+            return lstGioHang;
+        }
+        public ActionResult ThemGioHang(string sMaSach, string strURL)
+        {
+            List<GioHang> lstGioHang = LayGioHang();
+            GioHang sanpham = lstGioHang.Find(n => n.sMaSach == sMaSach);
+            if (sanpham == null)
+            {
+                sanpham = new GioHang(sMaSach);
+                lstGioHang.Add(sanpham);
+                return RedirectToAction("GioHangs", "GioHang");
+            }
+            else
+            {
+                sanpham.iSoLuong++;
+                return RedirectToAction("GioHangs", "GioHang");
+            }
+        }
+        private int TongSoLuong()
+        {
+            int iTongSoLuong = 0;
+            List<GioHang> lstGioHang = Session["Giohang"] as List<GioHang>;
+            if (lstGioHang != null)
+            {
+                iTongSoLuong = lstGioHang.Sum(n => n.iSoLuong);
+            }
+            return iTongSoLuong;
+        }
+        private double TongTien()
+        {
+            double iTongTien = 0;
+            List<GioHang> lstGioHang = Session["Giohang"] as List<GioHang>;
+            if (lstGioHang != null)
+            {
+                iTongTien = lstGioHang.Sum(n => n.dThanhTien);
+            }
+            return iTongTien;
+        }
+        public ActionResult GioHangs()
+        {
+            if (LayGioHang().Count == 0)
+            {
+                TempData["GioHangError"] = "Giỏ hàng của bạn chưa có gì.";
+                return RedirectToAction("Index", "Home");
+            }
+            var list = db.SACHes;
+            ViewBag.TongSoLuong = TongSoLuong();
+            ViewBag.TongTien = TongTien();
+            return View(list.ToList());
+        }
+        public ActionResult GioHangPartial()
+        {
+            ViewBag.TongSoLuong = TongSoLuong();
+            ViewBag.TongTien = TongTien();
+            return PartialView();
+        }
+        public ActionResult XoaGioHang(string sMaSP)
+        {
+            List<GioHang> lstGioHang = LayGioHang();
+
+            GioHang sanpham = lstGioHang.SingleOrDefault(n => n.sMaSach == sMaSP);
+
+            if (sanpham != null)
+            {
+                lstGioHang.RemoveAll(n => n.sMaSach == sMaSP);
+                return RedirectToAction("GioHangs");
+            }
+            if (lstGioHang.Count == 0)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            return RedirectToAction("GioHangs");
+        }
+        public ActionResult CapNhatGioHang(string sMaSP, FormCollection f)
+        {
+            List<GioHang> lstGioHang = LayGioHang();
+            GioHang sanpham = lstGioHang.SingleOrDefault(n => n.sMaSach == sMaSP);
+            if (sanpham != null)
+            {
+                sanpham.iSoLuong = int.Parse(f["txtSoluong"].ToString());
+            }
+            return RedirectToAction("GioHangs", "GioHang");
+        }
+        public ActionResult XoaTatCaGioHang()
+        {
+            List<GioHang> lstGioHang = LayGioHang();
+            lstGioHang.Clear();
+            return RedirectToAction("GioHangs", "GioHang");
+        }
+    }
+}
---
+++ QLBanSach/Models/GioHang.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace QLBanSach.Models
+{
+    public class GioHang
+    {
+        QLBS db = new QLBS();
+        public string sMaSach { set; get; }
+        public string sTenSach { set; get; }
+        public string sAnh { set; get; }
+        public double dDonGia { set; get; }
+        public int iSoLuong { set; get; }
+        public double dThanhTien { get { return dDonGia * iSoLuong; } }
+
+        public GioHang (string MaSach)
+        {
+            sMaSach = MaSach;
+            SACH s = db.SACHes.Single(x => x.masach == sMaSach);
+            sTenSach = s.tensach;
+            sAnh = s.anhbia;
+            dDonGia = double.Parse(s.dongiaban.ToString());
+            iSoLuong = 1;
+        }
+    }
+}
