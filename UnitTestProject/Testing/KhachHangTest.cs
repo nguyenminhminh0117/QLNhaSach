d93ca5ee18e11a3470b7334cfc5e44f1d50a4a18
VAIO\SONY <dotranthainam@gmail.com>
2017-11-15 04:36:46 +0700

    Unit test code
    example of unit test code

---
+++ UnitTestProject/Testing/KhachHangTest.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using QLBanSach.Controllers;
+using System.Web.Mvc;
+using QLBanSach.Models;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class KhachHangTest
+    {
+        [TestMethod]
+        public void XemChiTiet()
+        {
+            KhachHangController controller = new KhachHangController();
+
+            // Act
+            var result = controller.Details("KH_00001") as ViewResult;
+            KHACHHANG person = (KHACHHANG)result.Model;
+
+            // Assert
+            Assert.AreEqual("Nam", person.tenkh);
+
+        }
+        [TestMethod]
+        public void TaoTaiKhoanKH()
+        {
+            KhachHangController controller = new KhachHangController();
+            KHACHHANG KH = new KHACHHANG
+            {
+                makh = " ",
+                tenkh = "Minh khùng",
+                diachi = "115 Sư Vạn Hạnh nd",
+                sodienthoai = "0168430900",//Thử xóa 1 số (còn 9 số) thì nó sẽ chạy sai, tương tự với matkhaukh hoặc xóa @gmail.com đi
+                email = "minhminh@gmail.com",
+                matkhaukh = "1234567890"
+            };
+            // Act
+            var result = controller.Create(KH) as ViewResult;
+
+            // Assert
+            Assert.AreEqual(KH, KH);
+        }
+    }
+}
